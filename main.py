e1dea2ed7a9ad3a6f05b37dff7c2848f303f8b76
ArtemSavely <art.sav07@yandex.ru>
2025-05-02 17:45:42 +0300

    getting temperature from api

--- main.py
+++ main.py
@@ -1,27 +1,87 @@
 from flet import *
 import requests
 
+API_KEY = 'e68c4de4bbb92f4801d22d1a96059b1d'
+
+
+class StringField(Text):
+    def __init__(self, size, value, color=Colors.WHITE):
+        super().__init__()
+        self.value = value
+        self.size = size
+        self.color = color
+        self.weight = FontWeight.BOLD
+
+
+class CurrentWeather:
+    def __init__(
+            self,
+            city,
+            temperature,
+
+    ):
+        self.temperature = temperature
+        self.city = city
+
+
+class WeatherSearchBar(SearchBar):
+    def __init__(self, page):
+        super().__init__()
+        self.page = page
+        self.bar_hint_text = "Где посмотрим погоду?"
+        self.on_submit = self.get_weather
+        self.on_tap = lambda e: self.open_view()
+
+    def get_weather(self, e):
+        query = e.data
+        params = {
+            "lang": 'ru',
+            "access_key": API_KEY,
+            "query": query
+        }
+        request = requests.get(f"http://api.weatherstack.com/current", params=params).json()
+        temperature.value = request['current']['temperature']
+        print(request)
+        self.close_view()
+        self.page.update()
+
+
 
 
 # e68c4de4bbb92f4801d22d1a96059b1d
-API_KEY = 'e68c4de4bbb92f4801d22d1a96059b1d'
-params = {
-    "lang": 'ru',
-    "access_key": API_KEY,
-    "query": 'Москва'
-}
-request = requests.get(f"http://api.weatherstack.com/current", params=params).json()
-print(request)
-print(request['current']['weather_descriptions'])
-from translate import Translator
-translator= Translator(from_lang="english", to_lang="russian")
-translation = translator.translate('hi')
-print(translation)
+# API_KEY = 'e68c4de4bbb92f4801d22d1a96059b1d'
+# print(request['current']['weather_descriptions'])
+# from translate import Translator
+# translator= Translator(from_lang="english", to_lang="russian")
+# translation = translator.translate('hi')
+# print(translation)
 
-# def main(Page: page):
-#     page.title = "WeatherCheck"
-#     page.bgcolor = colors.BLACK
-#
-#
-#
-# flet.app(main)
+temperature = StringField(size=30, value="temp")
+city = StringField(size=25, value="city")
+
+Text()
+def main(page: Page):
+    page.title = "WeatherCheck"
+    page.bgcolor = Colors.BLUE
+    page.window.width = 350
+    page.window.height = 700
+    page.theme_mode = ThemeMode.DARK
+
+    search_bar = WeatherSearchBar(page=page)
+    page.add(
+        Container(
+            content=Column(
+                controls=[
+                    Container(
+                        border_radius=40,
+                        bgcolor=Colors.BLACK,
+                        content=search_bar
+                    ),
+                    city,
+                    temperature
+                ]
+            )
+        )
+    )
+
+app(target=main)
